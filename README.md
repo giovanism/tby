9836e0ec21cc675e77dbbd96da5adeec24489dac
Giovan Isa Musthofa <giovanmail@gmail.com>
2022-03-29 08:14:48 +0700

    Improve logging and add install method
    Signed-off-by: Giovan Isa Musthofa <giovanmail@gmail.com>

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Teleport tunnels manager
 
+## Install
+
+```sh
+go install github.com/giovanism/tby@latest
+```
+
 ## TODO
 
 - [ ] Daemonize tby as parent for tsh tunnel processes
--- tby.go
+++ tby.go
@@ -36,6 +36,9 @@ type SSHTunnel struct {
 }
 
 func (t SSHTunnel) runPgrep() (*exec.Cmd, error) {
+
+	log.Debug().Msgf("Running: pgrep -f %s", fmt.Sprintf("%d:localhost:%d", t.LocalPort, t.RemotePort))
+
 	cmd := exec.Command("pgrep", "-f", fmt.Sprintf("%d:localhost:%d", t.LocalPort, t.RemotePort))
 
 	err := cmd.Run()
@@ -47,6 +50,9 @@ func (t SSHTunnel) runPgrep() (*exec.Cmd, error) {
 }
 
 func (t SSHTunnel) runPkill() (*exec.Cmd, error) {
+
+	log.Debug().Msgf("Running: pkill -f %s", fmt.Sprintf("%d:localhost:%d", t.LocalPort, t.RemotePort))
+
 	cmd := exec.Command("pkill", "-f", fmt.Sprintf("%d:localhost:%d", t.LocalPort, t.RemotePort))
 
 	err := cmd.Run()
@@ -58,6 +64,7 @@ func (t SSHTunnel) runPkill() (*exec.Cmd, error) {
 }
 
 func (t SSHTunnel) IsUp() bool {
+
 	_, err := t.runPgrep()
 	if err != nil {
 		log.Err(err).Msg("Error running pgrep or process not found")
@@ -68,6 +75,9 @@ func (t SSHTunnel) IsUp() bool {
 }
 
 func (t SSHTunnel) Up() error {
+
+	log.Debug().Msgf("Starting: tsh ssh -NL %s %s", fmt.Sprintf("%d:localhost:%d", t.LocalPort, t.RemotePort), fmt.Sprintf("%s@%s", t.User, t.NodeName))
+
 	cmd := exec.Command("tsh", "ssh", "-NL", fmt.Sprintf("%d:localhost:%d", t.LocalPort, t.RemotePort), fmt.Sprintf("%s@%s", t.User, t.NodeName))
 
 	if err := cmd.Start(); err != nil {
@@ -87,6 +97,7 @@ func (t SSHTunnel) Up() error {
 }
 
 func (t SSHTunnel) Down() error {
+
 	_, err := t.runPkill()
 	if err != nil {
 		return err
@@ -96,6 +107,7 @@ func (t SSHTunnel) Down() error {
 }
 
 func getTbyDir() string {
+
 	var configDir, homeDir string
 
 	configDir, err := os.UserConfigDir()
@@ -117,6 +129,7 @@ func getTbyConfigPath() string {
 }
 
 func getTbyConfig() *TbyConfig {
+
 	data, err := os.ReadFile(configPath)
 	if err != nil {
 		log.Fatal().Msgf("Can't load tby config file: %v", err)
@@ -137,6 +150,7 @@ func main() {
 }
 
 func rootCmd() *cobra.Command {
+
 	cmd := &cobra.Command{
 		Use:   "tby ID",
 		Short: "Connect to tunnel ID",
@@ -163,7 +177,7 @@ An awesome terminal program that will accelerate your way of using tsh teleport 
 			log.Info().Msgf("Connecting to tunnel %d on port %d", id, tun.LocalPort)
 			err = tun.Up()
 			if err != nil {
-				log.Err(err).Msgf("Failed to connect to tunnel %d on port %d", id, tun.LocalPort)
+				log.Fatal().Err(err).Msgf("Failed to connect to tunnel %d on port %d", id, tun.LocalPort)
 			}
 		},
 	}
@@ -174,12 +188,14 @@ An awesome terminal program that will accelerate your way of using tsh teleport 
 }
 
 func downCmd() *cobra.Command {
+
 	return &cobra.Command{
 		Use:   "down ID",
 		Short: "Deactivate active tunnel",
 		Long:  `Deactivate active tunnel managed by tby.`,
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+
 			defer func() {
 				if err := recover(); err != nil {
 					log.Fatal().Msgf("recovered from panic: %v", err)
@@ -194,6 +210,7 @@ func downCmd() *cobra.Command {
 			tbyConfig := getTbyConfig()
 			tun := tbyConfig.Tunnels[id]
 
+			log.Info().Msgf("Disconnecting tunnel %d on port %d", id, tun.LocalPort)
 			err = tun.Down()
 			if err != nil {
 				log.Fatal().Err(err).Msgf("Failed to deactivate tunnel %d", id)
@@ -203,12 +220,14 @@ func downCmd() *cobra.Command {
 }
 
 func listCmd() *cobra.Command {
+
 	return &cobra.Command{
 		Use:     "list",
 		Aliases: []string{"ls"},
 		Short:   "List registered tunnels",
 		Long:    `List tunnels configured inside tby config file in a table.`,
 		Run: func(cmd *cobra.Command, args []string) {
+
 			tbyConfig := getTbyConfig()
 
 			t := tabby.New()
